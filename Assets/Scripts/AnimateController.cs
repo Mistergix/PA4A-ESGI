7c3e65b4958dfb08f8e6fef0230283cf78ec3abb
Neffros <46776595+Neffros@users.noreply.github.com>
2021-01-03 05:05:25 +0100

    functional basae vr tracking interactions and hand animations

--- Assets/Scripts/AnimateController.cs
+++ Assets/Scripts/AnimateController.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 using UnityEngine.XR;
 public class AnimateController : MonoBehaviour
 {
-
+    public bool showController;
     public InputDeviceCharacteristics controllerChars;
     public GameObject controllerPrefab;
+    public GameObject handPrefab;
     private InputDevice _controller;
-
+    private Animator _handAnimator;
+    
     private GameObject spawnedController;
+    private GameObject spawnedHand;
+
+    private static readonly int Trigger = Animator.StringToHash("Trigger");
+    private static readonly int Grip = Animator.StringToHash("Grip");
+
     // Start is called before the first frame update
     void Start()
+    {
+       TryInitialize();
+    }
+
+    void TryInitialize()
     {
         List<InputDevice> devices = new List<InputDevice>();
         InputDevices.GetDevicesWithCharacteristics(controllerChars, devices);
@@ -28,14 +40,56 @@ public class AnimateController : MonoBehaviour
             if (controllerPrefab)
             {
                 spawnedController = Instantiate(controllerPrefab, transform);
+                spawnedHand = Instantiate(handPrefab, transform);
+                _handAnimator = spawnedController.GetComponent<Animator>();
             }
         }
     }
 
+    void UpdateHandAnimator()
+    {
+        if (_controller.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            _handAnimator.SetFloat(Trigger, triggerValue);
+        }
+        else
+        {
+            _handAnimator.SetFloat(Trigger, triggerValue);
+        }
+        if (_controller.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            _handAnimator.SetFloat(Grip, gripValue);
+        }
+        else
+        {
+            _handAnimator.SetFloat(Grip, gripValue);
+        }
+    }
+
+    void UpdateActiveController()
+    {
+        if (showController)
+        {
+            spawnedHand.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedController.SetActive(false);
+            spawnedHand.SetActive(true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        _controller.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        if (!_controller.isValid)
+        {
+            TryInitialize();
+            return;
+        }
+        UpdateHandAnimator();  
+        UpdateActiveController();
+        /*_controller.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
         if (primaryButtonValue)
             Debug.Log("pressing main button in right controller");
 
@@ -45,6 +99,6 @@ public class AnimateController : MonoBehaviour
 
         _controller.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxis);
         if (primary2DAxis != Vector2.zero)
-            Debug.Log("moving stick");
+            Debug.Log("moving stick");*/
     }
 }
